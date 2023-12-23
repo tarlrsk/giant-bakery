48a82d7ddc7121c586e7b50dccf5fced670e320f
Pan <pan.1245.dev@gmail.com>
2023-12-23 16:32:27 +0700

    Implement signin for credentials

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,9 @@
-import NextAuth from "next-auth";
-import FacebookProvider from "next-auth/providers/facebook";
-import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { prisma } from "../../../../../prisma/client";
+import NextAuth, { NextAuthOptions } from "next-auth";
+import Facebook from "next-auth/providers/facebook";
+import Google from "next-auth/providers/google";
+import Credentials from "next-auth/providers/credentials";
 
 const FACEBOOK_CLIENT_ID = process.env.FACEBOOK_CLIENT_ID;
 const FACEBOOK_CLIENT_SECRET = process.env.FACEBOOK_CLIENT_SECRET;
@@ -18,20 +19,28 @@ if (!GOOGLE_CLIENT_ID || !GOOGLE_CLIENT_SECRET) {
   throw new Error("Missing Google oauth credentials");
 }
 
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
-    FacebookProvider({
+    Credentials({
+      name: "credentials",
+      credentials: {},
+      authorize(credentials, req) {
+        // TODO implement logic for signin.
+        throw new Error("Authorization has not been implemented.");
+      },
+    }),
+    Facebook({
       clientId: FACEBOOK_CLIENT_ID,
       clientSecret: FACEBOOK_CLIENT_SECRET,
     }),
-    GoogleProvider({
+    Google({
       clientId: GOOGLE_CLIENT_ID,
       clientSecret: GOOGLE_CLIENT_SECRET,
     }),
   ],
   callbacks: {
-    // fixing a bug in next-auth to assign id to user after login.
+    // fixing a bug in next-auth to assign id to user after signed in.
     async session({ session, user }: any) {
       if (session && user) {
         session.user.id = user.id;
@@ -40,6 +49,14 @@ export const authOptions = {
       return session;
     },
   },
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  // TODO add signin page.
+  //   pages: {
+  //     "signIn": "LoginPage"
+  //   }
 };
 
 export const handler = NextAuth(authOptions);
