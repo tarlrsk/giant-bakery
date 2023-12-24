f30f715846e08a9038cbc1bf4dba5e4dc18074b0
Piyaphon Wu <wupiyaphon@gmail.com>
2023-12-24 16:10:11 +0700

    updated: create address api

---
+++ src/app/api/customer-address/route.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "@/prisma/client";
+import { createAddressSchema } from "./types";
+
+// ----------------------------------------------------------------------
+
+export async function POST(req: NextRequest) {
+    if (req.method !== "POST")
+    return new NextResponse(null, { status: 404, statusText: "Not Found" });
+
+    try {
+        const {address, district, subDistrict, province, code, phone} = await req.json();
+
+        // const newAddress = await prisma.customerAddress.create({
+        //     data: {address, district, subDistrict, province, code, phone}
+        // })
+
+        console.log("req:", address, district, subDistrict, province, code)
+
+        return NextResponse.json({message: 'Successful'}, {status: 201})
+    } catch (err) {
+        return NextResponse.json({message: "An error occurred while signing up."}, {status: 500})
+    }
+}
---
+++ src/app/api/customer-address/types.ts
@@ -0,0 +1,7 @@
+import {z} from 'zod'
+
+export const createAddressSchema = z.object({
+    address: z.string().min(1).max(255),
+    district: z.string().min(1), subDistrict: z.string().min(1), province: z.string().min(1), code: z.string().min(1),
+phone: z.string().length(10, {message: 'Must be exactly 10 characters long'})
+})
