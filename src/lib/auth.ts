82d4e7912e2e558bd80fb65c6c2373a04e084641
Pan <pan.1245.dev@gmail.com>
2023-12-26 18:17:22 +0700

    Create auth lib

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,10 +1,9 @@
-import { Adapter } from "next-auth/adapters";
-import { NextAuthOptions } from "next-auth";
 import { prisma } from "@/lib/prisma";
 import { PrismaAdapter } from "@auth/prisma-adapter";
+import { NextAuthOptions } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
 import Facebook from "next-auth/providers/facebook";
 import Google from "next-auth/providers/google";
-import Credentials from "next-auth/providers/credentials";
 
 const NEXTAUTH_URL = process.env.NEXTAUTH_URL;
 const NEXTAUTH_SECRET = process.env.NEXTAUTH_SECRET;
@@ -34,49 +33,16 @@ export const authOptions: NextAuthOptions = {
   // },
   providers: [
     Credentials({
-      id: "admin-signin",
-      name: "Admin",
+      name: "Credentials",
       credentials: {
         username: { label: "Username", type: "text", placeholder: "username" },
         email: { label: "Email", type: "email", placeholder: "email" },
         password: { label: "Password", type: "password" },
       },
+
+      // TODO authorization
       async authorize(credentials, req) {
-        // Add logic here to look up the user from the credentials supplied
-        const user = { id: "1", name: "J Smith", email: "jsmith@example.com" };
-
-        if (user) {
-          // Any object returned will be saved in `user` property of the JWT
-          return user;
-        } else {
-          // If you return null then an error will be displayed advising the user to check their details.
-          return null;
-
-          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
-        }
-      },
-    }),
-    Credentials({
-      id: "customer-signin",
-      name: "Customer",
-      credentials: {
-        username: { label: "Username", type: "text", placeholder: "Username" },
-        email: { label: "Email", type: "email", placeholder: "Email" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials, req) {
-        // Add logic here to look up the user from the credentials supplied
-        const user = { id: "1", name: "J Smith", email: "jsmith@example.com" };
-
-        if (user) {
-          // Any object returned will be saved in `user` property of the JWT
-          return user;
-        } else {
-          // If you return null then an error will be displayed advising the user to check their details.
-          return null;
-
-          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
-        }
+        throw new Error("Not implemented");
       },
     }),
     Facebook({
