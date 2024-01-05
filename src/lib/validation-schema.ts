f79403d596fa401f055d8acddb9b2f73485ff2ef
Pan <pan.1245.dev@gmail.com>
2024-01-05 20:32:55 +0700

    Create validation schema for variants

--- src/lib/validation-schema.ts
+++ src/lib/validation-schema.ts
@@ -19,23 +19,42 @@ export const passwordValidationSchema = z
     },
   );
 
-// Customer Address ----------------------------------------------------------------------
+// Customer Address ----------------------------------------------------------
 
 export const customerAddressValidationSchema = z.object({
-  address: z.string().min(1).max(255),
-  district: z.string().min(1),
-  subdistrict: z.string().min(1),
-  province: z.string().min(1),
+  address: z.string({ required_error: "Address is required." }).min(3).max(255),
+  district: z
+    .string({ required_error: "District is required." })
+    .min(3)
+    .max(255),
+  subdistrict: z
+    .string({ required_error: "Subdistrict is required." })
+    .min(3)
+    .max(255),
+  province: z
+    .string({ required_error: "Province is required." })
+    .min(3)
+    .max(255),
   postcode: z
-    .string()
+    .string({ required_error: "Postcode is required." })
     .length(5)
     .refine((value) => isNumeric(value), {
       message: "Postcode must contain only numbers.",
     }),
   phone: z
-    .string()
+    .string({ required_error: "Phone is required." })
     .length(10, "Must be exactly 10 characters long")
     .refine((value) => isNumeric(value), {
       message: "Phone must contain only numbers.",
     }),
+});
+
+// Variants -------------------------------------------------------------------
+
+export const variantValidationSchema = z.object({
+  name: z.string({ required_error: "Name is required." }).min(3).max(255),
+  image: z.string().optional(),
+  type: z.enum(["BASE", "FILLINGS", "FROSTINGS", "CREAM"]),
+  isActive: z.string(),
+  isVisualize: z.string(),
 });
