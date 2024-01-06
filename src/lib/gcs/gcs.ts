e76f6f72f7070b39307790035afad3a1479a3690
Pan <pan.1245.dev@gmail.com>
2024-01-06 17:11:29 +0700

    Store bucket name in env

--- src/lib/gcs/gcs.ts
+++ src/lib/gcs/gcs.ts
@@ -2,12 +2,13 @@ import { Storage } from "@google-cloud/storage";
 
 const GCS_PROJECT_ID = process.env.GCS_PROJECT_ID;
 const GCS_KEYFILE = process.env.GCS_KEYFILE;
+const GCS_BUCKET = process.env.GCS_BUCKET as string;
 
 const storage = new Storage({
   projectId: GCS_PROJECT_ID,
   keyFilename: GCS_KEYFILE,
 });
 
-const bucket = storage.bucket("giant-bakery-dev");
+const bucket = storage.bucket(GCS_BUCKET);
 
 export { bucket };
