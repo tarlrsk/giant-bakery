dfb00265b8b8cf8b3cf7fb0462256aa5ee9c8a96
Thanatuch <thanatuch@greenmoons.co.th>
2024-01-07 20:12:12 +0700

    add cake crud

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -58,7 +58,7 @@ export async function POST(req: NextRequest) {
     return responseWrapper(
       500,
       null,
-      `Something went wrong./n Error: ${err.message}`,
+      `${err.message}`,
     );
   }
 }
