a710d215655f76636854b04159a5f39677952104
Pan <pan.1245.dev@gmail.com>
2024-01-10 19:05:56 +0700

    Configure role based authentication

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,9 +1,13 @@
 import NextAuth from "next-auth";
+import { JWT } from "next-auth/jwt";
+import type { User } from "@prisma/client";
 
 declare module "next-auth" {
   interface Session {
-    user: {
-      id: string;
-    } & DefaultSession["user"];
+    user: User & DefaultSession["user"];
   }
+}
+
+declare module "next-auth/jwt" {
+  type JWT = User;
 }
