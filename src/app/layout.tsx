ca96800eed84f6865f2ce0770ad667479df2dd01
Pan <pan.1245.dev@gmail.com>
2024-01-12 18:18:19 +0700

    Remove unnecessary variable

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const currentUser = await getCurrentUser();
+  await getCurrentUser();
   return (
     <html lang="en">
       <head>
