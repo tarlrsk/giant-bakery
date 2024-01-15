7012aaa87c0717f39e8a7ce5cf725ae79fcaecfa
Thanatuch <thanatuch.2003@gmail.com>
2024-01-15 16:24:07 +0700

    recover middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,34 +6,34 @@ const allowedOrigins =
     ? ["https://giantbakery.vercel.app", "https://giantbakery-uat.vercel.app"]
     : ["https://giantbakery-stg.vercel.app", "http://localhost:3000"];
 
-// export default withAuth(
-export default function middleware(req: NextRequest) {
-  const origin = req.headers.get("origin");
+export default withAuth(
+  function middleware(req: NextRequestWithAuth) {
+    const origin = req.headers.get("origin");
 
-  if (origin && !allowedOrigins.includes(origin)) {
-    return new NextResponse(null, {
-      status: 400,
-      statusText: "Bad Request.",
-      headers: { "Content-Type": "text/plain" },
-    });
-  }
+    if (origin && !allowedOrigins.includes(origin)) {
+      return new NextResponse(null, {
+        status: 400,
+        statusText: "Bad Request.",
+        headers: { "Content-Type": "text/plain" },
+      });
+    }
 
-  // if (
-  //   (req.nextUrl.pathname.startsWith("/api/admin") ||
-  //     req.nextUrl.pathname.startsWith("/admin")) &&
-  //   req.nextauth.token?.role !== "ADMIN"
-  // )
-  //   return NextResponse.rewrite(new URL("/denied", req.url));
+    if (
+      (req.nextUrl.pathname.startsWith("/api/admin") ||
+        req.nextUrl.pathname.startsWith("/admin")) &&
+      req.nextauth.token?.role !== "ADMIN"
+    )
+      return NextResponse.rewrite(new URL("/denied", req.url));
 
-  console.log(req.method);
-  console.log(req.url);
-}
-// {
-//   callbacks: {
-//     authorized: async ({ token }) => !!token,
-//   },
-// },
-// );
+    console.log(req.method);
+    console.log(req.url);
+  },
+  {
+    callbacks: {
+      authorized: async ({ token }) => !!token,
+    },
+  },
+);
 
 export const config = {
   matcher: ["/api/admin/:path*", "/admin/:path*"],
