6b8b2bb33e728d4d7eb89456c360dd332b3ce634
Pan <pan.1245.dev@gmail.com>
2024-01-16 18:10:14 +0700

    fix: config tag in FeatureSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
+import Wave from "@/components/Wave";
+import FeatureSection from "@/components/FeatureSection";
+
 // ----------------------------------------------------------------------
 
 export default function Home() {
   return (
-    <section className="h-screen flex items-center justify-center">
-      <div></div>
+    <section>
+      <Wave />
+      <div className="relative w-screen h-auto pl-36 py-28">
+        <FeatureSection
+          hasTag={false}
+          title="ไดเอ็ตแคมป์ริกเตอร์ ฮอตแซ็กโซโฟนฟินิกซ์เทค"
+          desc="ไดเอ็ตแคมป์ริกเตอร์ ฮอตแซ็กโซโฟนฟินิกซ์เทควันโดมะกัน"
+          btnLabel="สั่งเลย"
+        />
+      </div>
+      <div className="relative flex flex-col py-28 w-screen h-auto items-center justify-center gap-10">
+        <h1 className="font-semibold text-5xl">สินค้าขายดี</h1>
+        <p className="font-normal text-2xl">
+          ไดเอ็ตแคมป์ริกเตอร์ ฮอตแซ็กโซโฟนฟินิกซ์เทควันโดมะกัน
+        </p>
+      </div>
     </section>
   );
 }
--- src/components/FeatureSection.tsx
+++ src/components/FeatureSection.tsx
@@ -3,31 +3,38 @@ import React from "react";
 import { Button } from "@nextui-org/react";
 
 type Props = {
+  hasTag?: boolean;
   tag?: string;
   title: string;
   desc: string;
   btnLabel: string;
 };
 
-export default function FeatureSection({ tag, title, desc, btnLabel }: Props) {
+export default function FeatureSection({
+  hasTag = true,
+  tag,
+  title,
+  desc,
+  btnLabel,
+}: Props) {
   return (
-    <section>
-      <div className="relative">
-        <div className="flex flex-col w-1/3 items-start justify-center gap-6 relative">
-          <div className="flex flex-col items-start justify-center relative">
-            <p className="text-secondaryT-main text-xl font-semibold leading-normal">
-              {tag ?? "คุเคะโด"}
-            </p>
-            <h3 className="mt-[-8px] text-black text-5xl relative font-semibold leading-normal">
-              {title}
-            </h3>
-          </div>
-          <p className="relative font-normal text-black text-xl">{desc}</p>
-          <Button className="w-32 h-10 bg-secondaryT-main text-white text-base font-semibold rounded-full relative">
-            {btnLabel}
-          </Button>
-        </div>
+    <div className="flex flex-col w-2/5 items-start justify-center gap-6 relative">
+      <div className="flex flex-col items-start justify-center relative">
+        {hasTag === true ? (
+          <p className="text-secondaryT-main text-xl font-semibold leading-normal">
+            {tag ?? "คุเคะโด"}
+          </p>
+        ) : (
+          <></>
+        )}
+        <h1 className="text-black text-5xl relative font-semibold leading-normal">
+          {title}
+        </h1>
       </div>
-    </section>
+      <p className="relative font-normal text-black text-xl">{desc}</p>
+      <Button className="w-36 h-12 bg-secondaryT-main text-white text-xl font-semibold rounded-full relative">
+        {btnLabel}
+      </Button>
+    </div>
   );
 }
