ff8fa6ca767a2b9133e14149c698f2b2daa7be2c
Pan <pan.1245.dev@gmail.com>
2024-01-18 17:41:36 +0700

    fix: responsive wave bg

--- src/components/Wave.tsx
+++ src/components/Wave.tsx
@@ -4,12 +4,12 @@ import { Image } from "@nextui-org/react";
 
 export default function Wave() {
   return (
-    <div className="absolute top-0">
+    <div className="absolute top-0 left-0 w-full h-full">
       <Image
         src={"curve.svg"}
         alt="curve"
-        width={1800}
-        height={100}
+        width={"100%"}
+        height={"100vh"}
         className="z-0"
       />
     </div>
