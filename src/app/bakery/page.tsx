6d5ddc334f443e7c07ee68257bb26388e9466c4d
Pan <pan.1245.dev@gmail.com>
2024-01-26 12:05:41 +0700

    style: create title section for each product type

--- src/app/bakery/page.tsx
+++ src/app/bakery/page.tsx
@@ -1,5 +1,17 @@
 import React from "react";
+import TitleSection from "@/components/TitleSection";
 
 export default function BakeryPage() {
-  return <div></div>;
+  return (
+    <section>
+      <TitleSection title={"เบเกอรี่"} />
+      <div className="relative flex flex-col justify-center items-center">
+        <div className="relative flex gap-5">
+          <p>1</p>
+          <p>2</p>
+          <p>3</p>
+        </div>
+      </div>
+    </section>
+  );
 }
---
+++ src/components/TitleSection.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import Wave from "./Wave";
+
+type Props = {
+  title: string;
+};
+
+export default function TitleSection({ title }: Props) {
+  return (
+    <div>
+      <Wave />
+      <div className="relative flex flex-col justify-center items-center pt-36 py-72">
+        <h1 className="text-8xl font-bold text-primaryT-darker leading-normal">
+          {title}
+        </h1>
+        <p className="text-5xl font-semibold text-primaryT-dark">
+          CUKEDOH BAKERY
+        </p>
+      </div>
+    </div>
+  );
+}
