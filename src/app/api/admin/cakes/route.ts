62bc50e7b2e1e4f1c8d227573a82cd21b220ae58
Thanatuch <thanatuch.2003@gmail.com>
2024-01-28 23:22:32 +0700

    add cake image

--- src/app/api/admin/cakes/route.ts
+++ src/app/api/admin/cakes/route.ts
@@ -2,6 +2,11 @@ import { prisma } from "@/lib/prisma";
 import { NextRequest } from "next/server";
 import { cakeValidationSchema } from "@/lib/validationSchema";
 import { responseWrapper } from "@/utils/api-response-wrapper";
+import { parseBoolean } from "@/lib/parseBoolean";
+import { CakeType } from "@prisma/client";
+import { getFileUrl } from "@/lib/gcs/getFileUrl";
+import { bucket } from "@/lib/gcs/gcs";
+import { formatDate } from "@/lib/formatDate";
 
 // ----------------------------------------------------------------------
 
@@ -24,14 +29,34 @@ export async function GET(_req: NextRequest) {
 
 export async function POST(req: NextRequest) {
   try {
-    const body = await req.json();
-    const validation = cakeValidationSchema.safeParse(body);
+    const formData = await req.formData();
+
+    const name = formData.get("name") as string;
+    const description = formData.get("description") as string;
+    const type = formData.get("type") as CakeType;
+    const weight = parseFloat(formData.get("weight") as string);
+    const height = parseFloat(formData.get("height") as string);
+    const length = parseFloat(formData.get("length") as string);
+    const width = parseFloat(formData.get("width") as string);
+    const price = parseFloat(formData.get("price") as string);
+    const isActive = parseBoolean(formData.get("isActive") as string);
+    const variantIds = formData.getAll("variantIds") as string[];
+
+    const validation = cakeValidationSchema.safeParse({
+      name,
+      type,
+      price,
+      weight,
+      height,
+      length,
+      width,
+      isActive,
+      variantIds,
+    });
 
     if (!validation.success) {
       return responseWrapper(400, null, validation.error.format());
     }
-
-    const { variantIds } = body;
 
     for (var variantId of variantIds) {
       let variant = await prisma.variant.findUnique({
@@ -47,8 +72,48 @@ export async function POST(req: NextRequest) {
       }
     }
 
-    const newCake = await prisma.cake.create({
-      data: body,
+    const image = formData.get("image") as File | null;
+
+    if (!image) {
+      return responseWrapper(400, null, "Invalid image file.");
+    }
+
+    const imageFileName = `${formatDate(
+      new Date(Date.now()).toString(),
+    )}_${image.name.replace(/\s/g, "_")}`;
+
+    let newCake = await prisma.cake.create({
+      data: {
+        name: name,
+        description: description,
+        type: type,
+        price: price,
+        weight: weight,
+        height: height,
+        length: length,
+        width: width,
+        isActive: isActive,
+        variantIds: variantIds,
+      },
+    });
+
+    const buffer = Buffer.from(await image.arrayBuffer());
+
+    const imagePath = `cakes/${type}/${newCake.id}/${imageFileName}`;
+
+    const gcsFile = bucket.file(imagePath);
+
+    await gcsFile.save(buffer, {
+      metadata: {
+        contentType: image.type,
+      },
+    });
+
+    const imageUrl = await getFileUrl(imagePath);
+
+    newCake = await prisma.cake.update({
+      where: { id: newCake.id },
+      data: { image: imageUrl },
     });
 
     return responseWrapper(201, newCake, null);
