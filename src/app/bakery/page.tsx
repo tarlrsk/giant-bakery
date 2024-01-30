a49c937267e733e92c94c4fdb4ddb887539e2517
Pan <pan.1245.dev@gmail.com>
2024-01-30 11:09:03 +0700

    feat: implement basic tab

--- src/app/bakery/page.tsx
+++ src/app/bakery/page.tsx
@@ -1,5 +1,17 @@
+"use client";
+
 import React from "react";
 import TitleSection from "@/components/TitleSection";
+
+import { Tab, Tabs } from "@nextui-org/react";
+
+const TAB_ITEMS = [
+  { key: "all", title: "All" },
+  { key: "bread", title: "Bread" },
+  { key: "pie", title: "Pie" },
+  { key: "cookie", title: "Cookie" },
+  { key: "snack", title: "Snack" },
+];
 
 export default function BakeryPage() {
   return (
@@ -7,9 +19,13 @@ export default function BakeryPage() {
       <TitleSection title={"เบเกอรี่"} />
       <div className="relative flex flex-col justify-center items-center">
         <div className="relative flex gap-5">
-          <p>1</p>
-          <p>2</p>
-          <p>3</p>
+          <Tabs
+            variant={"underlined"}
+            aria-label="Tabs variants"
+            items={TAB_ITEMS}
+          >
+            {(item) => <Tab key={item.key} title={item.title} />}
+          </Tabs>
         </div>
       </div>
     </section>
