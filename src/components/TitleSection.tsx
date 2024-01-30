83e9f4c00570615e7f14530bac066ac2f568fcf2
Pan <pan.1245.dev@gmail.com>
2024-01-30 17:29:13 +0700

    style: adjust text layout

--- src/components/TitleSection.tsx
+++ src/components/TitleSection.tsx
@@ -6,15 +6,15 @@ type Props = {
   title: string;
 };
 
-export default function TitleSection({ title }: Props) {
+export default async function TitleSection({ title }: Props) {
   return (
     <div>
       <Wave />
       <div className="relative flex flex-col justify-center items-center pt-36 py-72">
-        <h1 className="text-8xl font-bold text-primaryT-darker leading-normal">
+        <h1 className="text-8xl font-bold text-center text-primaryT-darker leading-normal">
           {title}
         </h1>
-        <p className="text-5xl font-semibold text-primaryT-dark opacity-50">
+        <p className="text-5xl font-semibold text-center text-primaryT-dark opacity-50">
           CUKEDOH BAKERY
         </p>
       </div>
