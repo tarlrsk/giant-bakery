d082a500636d9a99b25d99ec3ca6e86434d82dc3
Pan <pan.1245.dev@gmail.com>
2024-01-30 19:53:51 +0700

    feat: main bakery page

--- src/app/bakery/page.tsx
+++ src/app/bakery/page.tsx
@@ -1,38 +1,13 @@
-"use client";
-
 import React from "react";
+import BakeryTab from "@/components/BakeryTab";
 import TitleSection from "@/components/TitleSection";
-
-import { Tab, Tabs } from "@nextui-org/react";
-
-const TAB_ITEMS = [
-  { key: "all", title: "All" },
-  { key: "bread", title: "Bread" },
-  { key: "pie", title: "Pie" },
-  { key: "cookie", title: "Cookie" },
-  { key: "snack", title: "Snack" },
-];
 
 export default function BakeryPage() {
   return (
     <section>
       <TitleSection title={"เบเกอรี่"} />
-      <div className="relative flex flex-col justify-center items-center pt-5 pb-24">
-        <div className="relative flex">
-          <Tabs
-            variant={"underlined"}
-            aria-label="Tabs variants"
-            items={TAB_ITEMS}
-          >
-            {(item) => (
-              <Tab
-                key={item.key}
-                title={item.title}
-                className="text-2xl font-semibold px-12"
-              />
-            )}
-          </Tabs>
-        </div>
+      <div className="relative flex flex-col justify-center items-center">
+        <BakeryTab />
       </div>
     </section>
   );
