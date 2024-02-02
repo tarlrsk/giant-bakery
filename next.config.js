ebc73d7efefbac4a70e29e5bb82721db7ba67592
Pan <pan.1245.dev@gmail.com>
2024-02-02 10:06:40 +0700

    config: add gcs hostname into next js config file

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ["storage.googleapis.com"],
+  },
   webpack: (config) => {
     config.resolve.fallback = {
       aws4: false,
