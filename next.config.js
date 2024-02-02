1010349d6fec47d51f2d158e5e980fff6a299e63
Pan <pan.1245.dev@gmail.com>
2024-02-02 10:36:27 +0700

    fix: image.domains to image.remotePatterns

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["storage.googleapis.com"],
+    remotePatterns: ["storage.googleapis.com/*"],
   },
   webpack: (config) => {
     config.resolve.fallback = {
