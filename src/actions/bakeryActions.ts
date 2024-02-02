f487202ade295ae53da8509a41900adf2613fab3
Pan <pan.1245.dev@gmail.com>
2024-02-02 11:04:16 +0700

    fix: revalidate path after calling api

--- src/actions/bakeryActions.ts
+++ src/actions/bakeryActions.ts
@@ -1,15 +1,19 @@
 "use server";
 
-import paths from "@/utils/api-path";
+import paths from "@/lib/paths";
+import apiPaths from "@/utils/api-path";
+import { revalidatePath } from "next/cache";
 
 // ----------------------------------------------------------------------
 
 export async function getBakeryByCategory(category: string) {
-  const { getBakeryByCat } = paths();
+  const { getBakeryByCat } = apiPaths();
 
   const res = await fetch(getBakeryByCat(category));
 
   const data = await res.json();
+
+  revalidatePath(paths.bakeryList());
 
   return data;
 }
