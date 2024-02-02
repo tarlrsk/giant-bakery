ee961f5dd3ba04b301f0cfd294f6117e2d31eff4
Pan <pan.1245.dev@gmail.com>
2024-02-02 11:03:49 +0700

    fix: image.remotePatterns wrongly formatted

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: ["storage.googleapis.com/*"],
+    remotePatterns: [
+      {
+        hostname: "storage.googleapis.com",
+      },
+    ],
   },
   webpack: (config) => {
     config.resolve.fallback = {
