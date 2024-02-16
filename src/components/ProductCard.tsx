17c1f3af95eac6cac40a3c460fd011b13935a296
Pan <pan.1245.dev@gmail.com>
2024-02-16 15:11:16 +0700

    fix: product card padding

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -45,7 +45,7 @@ export default function ProductCard({
           className=" object-cover mb-4 rounded-sm"
         />
         <article
-          className={`flex flex-col flex-wrap text-pretty px-6 gap-${
+          className={`flex flex-col flex-wrap text-pretty p-6 gap-${
             size === "sm" ? "1" : "2 items-center"
           }`}
         >
