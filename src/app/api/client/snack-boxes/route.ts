e059e906a6c9e4e97f978e1869d9b7d27ca603f9
Thanatuch <thanatuch.2003@gmail.com>
2024-02-18 02:20:40 +0700

    add preset snack box for client

---
+++ src/app/api/client/snack-boxes/route.ts
@@ -0,0 +1,42 @@
+import { prisma } from "@/lib/prisma";
+import { NextRequest } from "next/server";
+import { getFileUrl } from "@/lib/gcs/getFileUrl";
+import { responseWrapper } from "@/utils/api-response-wrapper";
+import { SnackBoxType } from "@prisma/client";
+
+export async function GET(_req: NextRequest) {
+  try {
+    const snackBoxes = await prisma.snackBox.findMany({
+      where: {
+        isActive: true,
+        isDeleted: false,
+        type: SnackBoxType.PRESET,
+      },
+      include: {
+        refreshments: {
+          include: {
+            refreshment: true,
+          },
+        },
+      },
+    });
+
+    for (var snackBox of snackBoxes) {
+      if (snackBox.imagePath) {
+        (snackBox as any).image = await getFileUrl(snackBox.imagePath);
+      }
+
+      for (var snackBoxRefreshment of snackBox.refreshments) {
+        if (snackBoxRefreshment.refreshment.imagePath) {
+          snackBoxRefreshment.refreshment.image = await getFileUrl(
+            snackBoxRefreshment.refreshment.imagePath,
+          );
+        }
+      }
+    }
+
+    return responseWrapper(200, snackBoxes, null);
+  } catch (err: any) {
+    return responseWrapper(500, null, err.message);
+  }
+}
