026a0217bf8fb2cd2d5126a58e734ee6e9071636
Pan <pan.1245.dev@gmail.com>
2024-02-18 15:28:12 +0700

    feat: preset snack box pagination

--- src/app/(client)/snack-box/page.tsx
+++ src/app/(client)/snack-box/page.tsx
@@ -1,12 +1,20 @@
 import React from "react";
+import TitleSection from "@/components/TitleSection";
 import CustomSnackBox from "@/components/custom-snack-box/CustomSnackBox";
+import SnackBoxPaginationContainer from "@/components/SnackBoxPaginationContainer";
 
 // ----------------------------------------------------------------------
 
 export default function SnackBoxPage() {
   return (
-    <div>
-      <CustomSnackBox />
-    </div>
+    <section>
+      <TitleSection title={"ชุดเบรก"} />
+      <div className="pb-20">
+        <SnackBoxPaginationContainer />
+      </div>
+      <div className="pb-20">
+        <CustomSnackBox />
+      </div>
+    </section>
   );
 }
