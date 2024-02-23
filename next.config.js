063c962109a9e7d30fdf022e39b8a93f6c2c5561
Thanatuch <thanatuch.2003@gmail.com>
2024-02-23 11:54:58 +0700

    remove image optimized

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "storage.googleapis.com",
-      },
-    ],
+    unoptimized: true,
   },
   webpack: (config) => {
     config.resolve.fallback = {
