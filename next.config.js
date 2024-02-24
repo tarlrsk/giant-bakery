61efbc22887c00c07a4dd17630ad652894c33efc
Thanatuch <thanatuch.2003@gmail.com>
2024-02-24 23:29:52 +0700

    use google cloud storage in serverManification

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
     return config;
   },
   experimental: {
-    serverMinification: false,
+    serverMinification: ["@google-cloud/storage"],
   },
   async headers() {
     return [
