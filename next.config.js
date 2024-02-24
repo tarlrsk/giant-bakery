adaa3233e6ae8e4a1b8a265bd35aa2c2bc0bbb17
Thanatuch <thanatuch.2003@gmail.com>
2024-02-24 23:31:40 +0700

    return to false

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
     return config;
   },
   experimental: {
-    serverMinification: ["@google-cloud/storage"],
+    serverMinification: "false",
   },
   async headers() {
     return [
