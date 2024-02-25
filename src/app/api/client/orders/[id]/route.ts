d016c7baec36a6ab6d96f1ff342e4a8838907b21
Thanatuch <thanatuch.2003@gmail.com>
2024-02-25 23:35:20 +0700

    add client get order by orderId

--- src/app/api/client/orders/[id]/route.ts
+++ src/app/api/client/orders/[id]/route.ts
@@ -2,10 +2,12 @@ import { NextRequest } from "next/server";
 import { responseWrapper } from "@/utils/api-response-wrapper";
 import { prismaOrder } from "@/persistence/order";
 import {
+  CartItemType,
   OrderStatus,
   PaymentMethod,
   PaymentType,
   ReceivedVia,
+  SnackBox,
 } from "@prisma/client";
 
 type GetOrderById = {
@@ -30,7 +32,7 @@ type OrderDetail = {
   discountPrice: number;
   paid: number;
   remaining: number;
-  items: Items;
+  items: Item[] | any;
   address: {
     address: string;
     district: string;
@@ -40,11 +42,13 @@ type OrderDetail = {
   } | null;
 };
 
-type Items = {
+type Item = {
   name: string;
   quantity: number;
+  type: CartItemType;
   price: number;
   pricePer: number;
+  subItem: string[];
 };
 
 export async function GET(req: NextRequest, { params }: GetOrderById) {
@@ -55,13 +59,20 @@ export async function GET(req: NextRequest, { params }: GetOrderById) {
       return responseWrapper(404, null, `Order with ID ${id} is not found.`);
     }
 
-    const orderResponse: OrderDetail = {
+    var paid = 0;
+    const paymentTypes: PaymentMethod[] = [];
+    for (var payment of order.payment) {
+      paid = paid + payment.amount;
+      paymentTypes.push(payment.type);
+    }
+
+    const data: OrderDetail = {
       orderId: order.id,
       firstName: order.cFirstName,
       lastName: order.cLastName,
       phone: order.phone ? order.phone : "",
       orderedAt: order.orderedAt,
-      paymentMethod: order.payment.map((p) => p.type),
+      paymentMethod: paymentTypes,
       receivedVia: order.receivedVia,
       totalPrice: order.totalPrice,
       status: order.status,
@@ -69,18 +80,89 @@ export async function GET(req: NextRequest, { params }: GetOrderById) {
       remark: order.remark,
       shippingFee: order.shippingFee,
       discountPrice: order.discountPrice,
-      paid: 1,
-      remaining: 0,
-      items: {
-        name: "",
-        quantity: 0,
-        price: 0,
-        pricePer: 0,
-      },
+      paid: paid,
+      remaining: order.totalPrice - paid,
+      items: [],
       address: null,
     };
 
-    return responseWrapper(200, null, null);
+    if (order.receivedVia == ReceivedVia.DELIVERY) {
+      data.address = {
+        address: order.address!,
+        district: order.district!,
+        subdistrict: order.subdistrict!,
+        province: order.province!,
+        postcode: order.postcode!,
+      };
+    }
+
+    const items: Item[] = [];
+
+    for (let cake of order.orderCake) {
+      let item: Item = {
+        name: cake.name,
+        quantity: cake.quantity,
+        price: cake.price,
+        pricePer: cake.pricePer,
+        subItem: [],
+        type: CartItemType.CAKE,
+      };
+      if (cake.size) {
+        item.subItem.push(cake.size);
+      }
+      if (cake.base) {
+        item.subItem.push(cake.base);
+      }
+      if (cake.filling) {
+        item.subItem.push(cake.filling);
+      }
+      if (cake.cream) {
+        item.subItem.push(cake.cream);
+      }
+      if (cake.topEdge) {
+        item.subItem.push(cake.topEdge);
+      }
+      if (cake.bottomEdge) {
+        item.subItem.push(cake.bottomEdge);
+      }
+      if (cake.surface) {
+        item.subItem.push(cake.surface);
+      }
+      // TODO Color Cake
+      items.push(item);
+    }
+
+    for (let refreshment of order.orderRefreshment) {
+      let item: Item = {
+        name: refreshment.name,
+        quantity: refreshment.quantity,
+        price: refreshment.price,
+        pricePer: refreshment.pricePer,
+        subItem: [],
+        type: CartItemType.REFRESHMENT,
+      };
+
+      items.push(item);
+    }
+
+    for (let snackBox of order.orderSnackBox) {
+      let item: Item = {
+        name: snackBox.name,
+        quantity: snackBox.quantity,
+        price: snackBox.price,
+        pricePer: snackBox.pricePer,
+        subItem: [],
+        type: CartItemType.SNACK_BOX,
+      };
+
+      for (let refreshment of snackBox.refreshments) {
+        item.subItem.push(refreshment.name);
+      }
+
+      data.items = items;
+    }
+
+    return responseWrapper(200, items, null);
   } catch (err: any) {
     return responseWrapper(500, null, err);
   }
