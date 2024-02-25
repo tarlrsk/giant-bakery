a44e2a3a1e1bf9e8230bb6dbeaa27e64a347995f
Thanatuch <thanatuch.2003@gmail.com>
2024-02-25 16:51:23 +0700

    add client get all variants

---
+++ src/app/api/client/variants/route.ts
@@ -0,0 +1,126 @@
+import { prisma } from "@/lib/prisma";
+import { NextRequest } from "next/server";
+import { getFileUrl } from "@/lib/gcs/getFileUrl";
+import { responseWrapper } from "@/utils/api-response-wrapper";
+import {
+  MasterCakeBase,
+  MasterCakeBottomEdge,
+  MasterCakeCream,
+  MasterCakeDecoration,
+  MasterCakeFilling,
+  MasterCakeSize,
+  MasterCakeSurface,
+  MasterCakeTopEdge,
+} from "@prisma/client";
+
+enum VariantType {
+  SIZE = "SIZE",
+  BASE = "BASE",
+  FILLING = "FILLING",
+  CREAM = "CREAM",
+  TOP_EDGE = "TOP_EDGE",
+  BOTTOM_EDGE = "BOTTOM_EDGE",
+  DECORATION = "DECORATION",
+  SURFACE = "SURFACE",
+}
+
+export async function GET(req: NextRequest) {
+  try {
+    let variants: {
+      sizes: MasterCakeSize[];
+      bases: MasterCakeBase[];
+      fillings: MasterCakeFilling[];
+      creams: MasterCakeCream[];
+      topEdges: MasterCakeTopEdge[];
+      bottomEdges: MasterCakeBottomEdge[];
+      decorations: MasterCakeDecoration[];
+      surfaces: MasterCakeSurface[];
+    } = {
+      sizes: [],
+      bases: [],
+      fillings: [],
+      creams: [],
+      topEdges: [],
+      bottomEdges: [],
+      decorations: [],
+      surfaces: [],
+    };
+
+    variants.sizes = await prisma.masterCakeSize.findMany({
+      where: {
+        isDeleted: false,
+      },
+    });
+
+    variants.bases = await prisma.masterCakeBase.findMany({
+      where: {
+        isDeleted: false,
+      },
+    });
+
+    variants.fillings = await prisma.masterCakeFilling.findMany({
+      where: {
+        isDeleted: false,
+      },
+    });
+
+    variants.creams = await prisma.masterCakeCream.findMany({
+      where: {
+        isDeleted: false,
+      },
+    });
+    for (var variant of variants.creams) {
+      if (variant.imagePath) {
+        variant.image = await getFileUrl(variant.imagePath);
+      }
+    }
+
+    variants.topEdges = await prisma.masterCakeTopEdge.findMany({
+      where: {
+        isDeleted: false,
+      },
+    });
+    for (var variant of variants.topEdges) {
+      if (variant.imagePath) {
+        variant.image = await getFileUrl(variant.imagePath);
+      }
+    }
+
+    variants.bottomEdges = await prisma.masterCakeBottomEdge.findMany({
+      where: {
+        isDeleted: false,
+      },
+    });
+    for (variant of variants.bottomEdges) {
+      if (variant.imagePath) {
+        variant.image = await getFileUrl(variant.imagePath);
+      }
+    }
+
+    variants.decorations = await prisma.masterCakeDecoration.findMany({
+      where: {
+        isDeleted: false,
+      },
+    });
+    for (var variant of variants.decorations) {
+      if (variant.imagePath) {
+        variant.image = await getFileUrl(variant.imagePath);
+      }
+    }
+
+    variants.surfaces = (await prisma.masterCakeSurface.findMany({
+      where: {
+        isDeleted: false,
+      },
+    })) as any;
+    for (var variant of variants.surfaces) {
+      if (variant.imagePath) {
+        variant.image = await getFileUrl(variant.imagePath);
+      }
+    }
+
+    return responseWrapper(200, variants, null);
+  } catch (err: any) {
+    return responseWrapper(500, null, err.message);
+  }
+}
