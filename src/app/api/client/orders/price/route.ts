42b8e8428ab964148a8aa5a0ef66889968610cb8
Thanatuch <thanatuch.2003@gmail.com>
2024-02-26 22:27:44 +0700

    get price - addressId could be null

--- src/app/api/client/orders/price/route.ts
+++ src/app/api/client/orders/price/route.ts
@@ -54,22 +54,10 @@ type Discounts = {
 
 export async function GET(req: NextRequest) {
   try {
-    const addressId = req.nextUrl.searchParams.get("addressId") as string;
+    const addressId = req.nextUrl.searchParams.get("addressId") as
+      | string
+      | null;
     const userId = req.nextUrl.searchParams.get("userId") as string;
-
-    const address = await prisma.customerAddress.findFirst({
-      where: {
-        id: addressId,
-      },
-    });
-
-    if (!address) {
-      return responseWrapper(
-        404,
-        null,
-        `Address with addressId ${addressId} is not found.`,
-      );
-    }
 
     const { getInterExpressLocation, getPrice, getCart } = apiPaths();
     // Get CartData
@@ -90,75 +78,92 @@ export async function GET(req: NextRequest) {
         },
       ],
       totalDiscount: 20,
+      shippingFee: 0,
     } as TotalPriceResponse;
 
-    // Get Destination Location
-    const res = await fetch(getInterExpressLocation(address.postcode));
-    const data = await res.json();
-    const filterDesData = filterDataByNameTh(
-      address.subdistrict,
-      address.district,
-      address.province,
-      data,
-    );
-
-    // Get Osg Location
-    const osgRes = await fetch(getInterExpressLocation("21000"));
-    const osgData = await osgRes.json();
-    const filterOsgData = filterDataByNameTh(
-      "เนินพระ",
-      "เมืองระยอง",
-      "ระยอง",
-      osgData,
-    );
-
-    // Map Get InterExpressPrice Body
-    const getPriceBody = {
-      orgSubDistricts: filterOsgData.subDistricts.id,
-      orgDistrict: filterOsgData.districts.id,
-      orgProvince: filterOsgData.provinces.id,
-      destDistrict: filterOsgData.districts.id,
-      destSubDistricts: filterOsgData.subDistricts.id,
-      destProvince: filterOsgData.provinces.id,
-      totalPackage: null,
-      orgPostCode: "21000",
-      orgZoneId: filterOsgData.zoneDesc.id,
-      destPostCode: address.postcode,
-      destZoneId: filterDesData.zoneDesc.id,
-      ItmsCustomerId: "0000000000000",
-      ItmsDcCode: filterDesData.subDistricts.dcCodeItms,
-      ItmsProvinceCode: filterDesData.subDistricts.provinceCodeItms,
-      ItmsDistrictCode: filterDesData.subDistricts.districtCodeItms,
-      PostalCode: address.postcode,
-      PaymentCode: "CR",
-      BoxDetails: [
-        {
-          ServiceId: 2,
-          ServiceControlId: 5,
-          TemperatureTypeId: 1,
-          TemperatureControlId: 2,
-          PackageCount: 1,
-          boxSize: "S1",
-          boxDesc: "≤ 5 กก. หรือ ≤ 7,500 ลบ.ซม.",
-          Weight: 0,
-          Width: 25,
-          Length: 20,
-          Height: 15,
+    if (addressId && addressId != "") {
+      const address = await prisma.customerAddress.findFirst({
+        where: {
+          id: addressId,
         },
-      ],
-    } as Shipment;
+      });
 
-    // Fetch InterExpress Get Price API
-    const getPriceRes = await fetch(getPrice(), {
-      method: "POST",
-      headers: new Headers({ "content-type": "application/json" }),
-      body: JSON.stringify(getPriceBody),
-    });
-    const getPriceData = await getPriceRes.json();
-    response.shippingFee = getPriceData.reduce(
-      (sum: any, box: any) => sum + box.totalRate,
-      0,
-    );
+      if (!address) {
+        return responseWrapper(
+          404,
+          null,
+          `Address with addressId ${addressId} is not found.`,
+        );
+      }
+
+      // Get Destination Location
+      const res = await fetch(getInterExpressLocation(address.postcode));
+      const data = await res.json();
+      const filterDesData = filterDataByNameTh(
+        address.subdistrict,
+        address.district,
+        address.province,
+        data,
+      );
+
+      // Get Osg Location
+      const osgRes = await fetch(getInterExpressLocation("21000"));
+      const osgData = await osgRes.json();
+      const filterOsgData = filterDataByNameTh(
+        "เนินพระ",
+        "เมืองระยอง",
+        "ระยอง",
+        osgData,
+      );
+
+      // Map Get InterExpressPrice Body
+      const getPriceBody = {
+        orgSubDistricts: filterOsgData.subDistricts.id,
+        orgDistrict: filterOsgData.districts.id,
+        orgProvince: filterOsgData.provinces.id,
+        destDistrict: filterOsgData.districts.id,
+        destSubDistricts: filterOsgData.subDistricts.id,
+        destProvince: filterOsgData.provinces.id,
+        totalPackage: null,
+        orgPostCode: "21000",
+        orgZoneId: filterOsgData.zoneDesc.id,
+        destPostCode: address.postcode,
+        destZoneId: filterDesData.zoneDesc.id,
+        ItmsCustomerId: "0000000000000",
+        ItmsDcCode: filterDesData.subDistricts.dcCodeItms,
+        ItmsProvinceCode: filterDesData.subDistricts.provinceCodeItms,
+        ItmsDistrictCode: filterDesData.subDistricts.districtCodeItms,
+        PostalCode: address.postcode,
+        PaymentCode: "CR",
+        BoxDetails: [
+          {
+            ServiceId: 2,
+            ServiceControlId: 5,
+            TemperatureTypeId: 1,
+            TemperatureControlId: 2,
+            PackageCount: 1,
+            boxSize: "S1",
+            boxDesc: "≤ 5 กก. หรือ ≤ 7,500 ลบ.ซม.",
+            Weight: 0,
+            Width: 25,
+            Length: 20,
+            Height: 15,
+          },
+        ],
+      } as Shipment;
+
+      // Fetch InterExpress Get Price API
+      const getPriceRes = await fetch(getPrice(), {
+        method: "POST",
+        headers: new Headers({ "content-type": "application/json" }),
+        body: JSON.stringify(getPriceBody),
+      });
+      const getPriceData = await getPriceRes.json();
+      response.shippingFee = getPriceData.reduce(
+        (sum: any, box: any) => sum + box.totalRate,
+        0,
+      );
+    }
 
     response.total =
       response.subTotal + response.shippingFee - response.totalDiscount;
