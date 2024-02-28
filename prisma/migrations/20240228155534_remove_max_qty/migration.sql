cc5592571899aeb288897da94dfc41e792b24dfd
Thanatuch <thanatuch.2003@gmail.com>
2024-02-28 22:55:44 +0700

    remove qty

---
+++ prisma/migrations/20240228155534_remove_max_qty/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `maxQty` on the `Refreshment` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Refreshment" DROP COLUMN "maxQty";
