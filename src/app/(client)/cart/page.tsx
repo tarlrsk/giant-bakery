4c8fe142fc531c73ee95a813f8fdacbb57100ea7
Piyaphon Wu <wupiyaphon@gmail.com>
2024-03-16 19:23:15 +0700

    updated increase decrease button and responsive

--- src/app/(client)/cart/page.tsx
+++ src/app/(client)/cart/page.tsx
@@ -4,12 +4,11 @@ import CartEmptyView from "@/components/CartEmptyView";
 import CartItemTable from "@/components/cart-table/CartItemTable";
 import { getCartData, updateCartItem } from "@/actions/cartActions";
 import CartSummaryTable from "@/components/cart-table/CartSummaryTable";
+import CheckoutButtonModal from "@/components/modal/CheckoutButtonModal";
 
 import { Button } from "@nextui-org/react";
 
 import { ICartItem } from "./types";
-import AuthModal from "@/components/modal/AuthModal";
-import CheckoutButtonModal from "@/components/modal/CheckoutButtonModal";
 
 // ----------------------------------------------------------------------
 
@@ -37,7 +36,7 @@ interface ICartResponse {
 export default function CartPage() {
   return (
     <div className="py-20">
-      <div className={`flex flex-col justify-center items-center gap-6`}>
+      <div className={`flex flex-col items-center justify-center gap-6`}>
         <ItemCartView />
       </div>
     </div>
@@ -60,19 +59,19 @@ async function ItemCartView() {
 
   return (
     <div className="container px-6">
-      <h1 className="text-2xl md:text-3xl font-medium text-left mb-4">
+      <h1 className="mb-4 text-left text-2xl font-medium md:text-3xl">
         ตะกร้าของฉัน
       </h1>
       {suggestDiscounts.length > 0 && (
-        <div className=" text-gray-500 mb-3 mt-5">
+        <div className=" mb-3 mt-5 text-gray-500">
           {suggestDiscounts.map((el: string, index: number) => (
-            <p key={index} className=" text-sm font-normal ">
+            <p key={index} className="text-xs  font-normal md:text-sm ">
               {el}
             </p>
           ))}
         </div>
       )}
-      <div className="grid grid-cols-1 md:grid-cols-6 gap-6">
+      <div className="grid grid-cols-1 gap-6 md:grid-cols-6">
         <div className="md:col-span-4 ">
           <CartItemTable
             userId={currentUser?.id || ""}
@@ -96,7 +95,7 @@ async function ItemCartView() {
               as={Link}
               color="secondary"
               size="lg"
-              className=" font-medium text-xl rounded-none"
+              className=" mt-4 rounded-none text-lg font-medium md:text-xl"
               fullWidth
             >
               ดำเนินการต่อ
