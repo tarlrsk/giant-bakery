a6a77fe9e1e8e18b434ad4e1c9ae8aee5178c7cd
Piyaphon Wu <wupiyaphon@gmail.com>
2024-03-21 09:59:06 +0700

    fixed custom cake bugs

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -3,7 +3,7 @@ export const AVAILABLE_COLORS = [
   { value: "PURPLE", label: "ม่วง", code: "#7758a9" },
   { value: "RED", label: "แดง", code: "#aa2639" },
   { value: "DARK_RED", label: "แดงคริสมาสต์", code: "#c30000" },
-  { value: "ORANGE", label: "ส้ม", code: "#F48c08" },
+  { value: "ORANGE", label: "ส้ม", code: "#f48c08" },
   { value: "YELLO_LEMON", label: "เหลืองมะนาว", code: "#e6e007" },
   { value: "DARK_GREEN", label: "เขียวแก่", code: "#2f5e1e" },
   { value: "GREEN_APPLE", label: "เขียวแอปเปิ้ล", code: "#80c06d" },
